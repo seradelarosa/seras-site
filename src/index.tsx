07b818d66cf272139eaf7cfe629a7d96e9c52ca0
sophiadelarosa <sophial.delarosa@gmail.com>
2024-12-04 21:47:11 -0500

    The mystery of the non-existent referenced file continues.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/style.css';
+// import './styles/style.css';
 
 // Initialize the root
 const rootElement = document.getElementById('root');
