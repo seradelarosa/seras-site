530c3d6f682e0baccbd6258b0b5e56a48af05621
Sera De La Rosa <sera.delarosa@outlook.com>
2025-01-29 11:54:08 -0500

    Cleaning up file structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import SeraPage from './components/Sera/sera'; // Correct path
-import SophiaPage from './components/Sophia/sophia'; // Correct path
-import UtopiaPage from './components/Utopia/utopia'; // Correct path
-import ShopPage from './components/Shop/shop'; // Correct path
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/seradelarosa" element={<SeraPage />} />
-        <Route path="/sophiadelarosa" element={<SophiaPage />} />
-        <Route path="/utopia" element={<UtopiaPage />} />
-        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/" element={<SeraPage />} />
       </Routes>
     </Router>
   );
